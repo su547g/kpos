1adeb7f893ecf22e20c15f0f7a30639a104daef0
kangwang1982@gmail.com <kangwang1982@gmail.com@ea9e98a1-8d46-a6bd-9028-500515dead74>
2012-03-20 03:22:04 +0000

    add PaymentRecord

--- src/com/kpos/domain/Bill.java
+++ src/com/kpos/domain/Bill.java
@@ -1,9 +1,12 @@
 package com.kpos.domain;
 
+import org.hibernate.annotations.ForeignKey;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kpos.
@@ -13,13 +16,6 @@ import javax.persistence.*;
 @Entity
 @Table(name = "BILL")
 public class Bill extends AbstractDomainObject {
-    public static enum PaymentType {
-        CASH, CARD;
-        @Override public String toString() {
-            return super.toString().toUpperCase();
-        }
-    }
-    
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "id")
@@ -34,14 +30,10 @@ public class Bill extends AbstractDomainObject {
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Order order;
 
-    @Column(name = "payment_type")
-    private String paymentType;
-
-    @Column(name = "card_type")
-    private String cardType;
-
-    @Column(name = "card_number")
-    private String cardNumber;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "bill", orphanRemoval = true, fetch = FetchType.LAZY)
+    @ForeignKey(name = "PAYMENT_2_BILL_FK")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<PaymentRecord> paymentRecords = new ArrayList<PaymentRecord>();
     
     public Long getId() {
         return id;
@@ -67,28 +59,12 @@ public class Bill extends AbstractDomainObject {
         this.order = order;
     }
 
-    public String getPaymentType() {
-        return paymentType;
+    public List<PaymentRecord> getPaymentRecords() {
+        return paymentRecords;
     }
 
-    public void setPaymentType(String paymentType) {
-        this.paymentType = paymentType;
-    }
-
-    public String getCardType() {
-        return cardType;
-    }
-
-    public void setCardType(String cardType) {
-        this.cardType = cardType;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
+    public void setPaymentRecords(List<PaymentRecord> paymentRecords) {
+        this.paymentRecords = paymentRecords;
     }
 
     @Override
@@ -101,9 +77,7 @@ public class Bill extends AbstractDomainObject {
         if (Double.compare(bill.amount, amount) != 0) return false;
         if (id != null ? !id.equals(bill.id) : bill.id != null) return false;
         if (order != null ? !order.equals(bill.order) : bill.order != null) return false;
-        if (paymentType != null ? !paymentType.equals(bill.paymentType) : bill.paymentType != null) return false;
-        if (cardType != null ? !cardType.equals(bill.cardType) : bill.cardType != null) return false;
-        if (cardNumber != null ? !cardNumber.equals(bill.cardNumber) : bill.cardNumber != null) return false;
+
         return true;
     }
 
@@ -115,9 +89,7 @@ public class Bill extends AbstractDomainObject {
         temp = amount != +0.0d ? Double.doubleToLongBits(amount) : 0L;
         result = 31 * result + (int) (temp ^ (temp >>> 32));
         result = 31 * result + (order != null ? order.hashCode() : 0);
-        result = 31 * result + (paymentType != null ? paymentType.hashCode() : 0);
-        result = 31 * result + (cardType != null ? cardType.hashCode() : 0);
-        result = 31 * result + (cardNumber != null ? cardNumber.hashCode() : 0);
+
         return result;
     }
 }
