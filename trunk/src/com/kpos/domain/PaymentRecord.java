bb069f38d01ac8e840477bb0c3a984db07887deb
kangwang1982@gmail.com <kangwang1982@gmail.com@ea9e98a1-8d46-a6bd-9028-500515dead74>
2012-03-20 03:22:04 +0000

    add PaymentRecord

---
+++ trunk/src/com/kpos/domain/PaymentRecord.java
@@ -0,0 +1,124 @@
+package com.kpos.domain;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+/**
+ * Created by kpos.
+ * Author: kkwang
+ * Date: 3/19/12 11:18 PM
+ */
+@Entity
+@Table(name = "PAYMENT_RECORD")
+public class PaymentRecord extends AbstractDomainObject {
+    public static enum PaymentType {
+        CASH, CARD;
+        @Override public String toString() {
+            return super.toString().toUpperCase();
+        }
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "amount")
+    private double amount;
+
+    //owning entity
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "bill_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Bill bill;
+
+    @Column(name = "payment_type")
+    private String paymentType;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PaymentRecord that = (PaymentRecord) o;
+
+        if (Double.compare(that.amount, amount) != 0) return false;
+        if (bill != null ? !bill.equals(that.bill) : that.bill != null) return false;
+        if (cardNumber != null ? !cardNumber.equals(that.cardNumber) : that.cardNumber != null) return false;
+        if (cardType != null ? !cardType.equals(that.cardType) : that.cardType != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (paymentType != null ? !paymentType.equals(that.paymentType) : that.paymentType != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id != null ? id.hashCode() : 0;
+        temp = amount != +0.0d ? Double.doubleToLongBits(amount) : 0L;
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (bill != null ? bill.hashCode() : 0);
+        result = 31 * result + (paymentType != null ? paymentType.hashCode() : 0);
+        result = 31 * result + (cardType != null ? cardType.hashCode() : 0);
+        result = 31 * result + (cardNumber != null ? cardNumber.hashCode() : 0);
+        return result;
+    }
+}
